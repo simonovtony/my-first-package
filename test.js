126e409cccbed51d98498c3ddc852ef4e747716f
Anton Simonov <simonovtony@outlook.com>
2017-05-17 01:25:27 +0300

    test

---
+++ test.js
@@ -0,0 +1,21 @@
+var inputChecker = require('inputcheck').inputChecker;
+
+var ic = new inputChecker('Shelley', 'output');
+
+ic.on('write', function (data) {
+    this.writeStream.write(data, 'utf8');
+});
+
+ic.addListener('echo', function (data) {
+    console.log(this.name + ' wrote ' + data);
+});
+
+ic.on('end', function () {
+    process.exit();
+});
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (input) {
+    ic.check(input);
+});
